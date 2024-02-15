358a0e82ee1b76b79812166d08c1ae7f8b73a3e0
Jennifer Yeaton <45831643+jkyeaton@users.noreply.github.com>
2024-02-15 12:33:09 -0600

    reading in reddit data (#2)
    <!---
    Please write your PR name in the present imperative tense. Examples of
    that tense are:
    "Fix issue in the dispatcher where…", "Improve our handling of…", etc."
    
    For more information on Pull Requests, you can reference here:
    
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## Describe your changes
    Notebook showing how to read in the reddit data to a pandas dataframe.
    
    ## Non-obvious technical information
    
    
    ## Checklist before requesting a review
    <!--- These are suggested things you could add, but what you add will be
    dependent on your repository's standards. --->
    - [x] The code runs successfully.
    
    ```commandline
    HERE IS SOME COMMAND LINE OUTPUT
    ```
    
    ---------
    
    Co-authored-by: Jennifer Yeaton <jenniferyeaton@Jennifers-MacBook-Pro-2.local>
    Co-authored-by: Chanteria Milner <chanteria.milner@gmail.com>

---
+++ project/cleaning/proquest_clean.py
@@ -0,0 +1,14 @@
+"""
+Creates a data cleaner class to clean the proquest data.
+
+Author(s): Chanteria Milner
+"""
+
+
+class ProquestCleaner:
+    """
+    Cleans data exported from Proquest TDM.
+
+    Args:
+        data (pandas.DataFrame): The data to clean.
+    """
