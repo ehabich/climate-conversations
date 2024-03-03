97d2d121a24ac39bb974bf43f7b3d868f946fc44
Kathryn Link-Oberstar <111700741+klinkoberstar@users.noreply.github.com>
2024-03-03 11:13:51 -0600

    Kathryn (#12)
    Updated MF classifier and dictionary generator
    
    ## Checklist before requesting a review
    <!--- These are suggested things you could add, but what you add will be
    dependent on your repository's standards. --->
    - [ ] The code runs successfully.
    
    =
    
    ---------
    
    Co-authored-by: Kathryn Jo Link-Oberstar <klinkoberstar@linux2.cs.uchicago.edu>

--- project/analysis/moral_strength_classifier.py
+++ project/analysis/moral_strength_classifier.py
@@ -1,77 +1,108 @@
-import os
-import sys
-import pickle
+import argparse
 import json
+import os
+import pickle
+import sys
+
 import numpy as np
 import pandas as pd
 from gensim.models import KeyedVectors
-import argparse
-from datetime import datetime
+
 
 # EXAMPLE CLI RUN
-# poetry run python moral_strength_classifier.py --subreddit worldnews --utc_start 1662654781 --utc_end 1662703120 --rows 0,200
+# poetry run python moral_strength_classifier.py --filepath project/data_collection/project_data/tokenized_climate_comments.pickle --col_to_tokenize body --subreddit climate --tokenize False --type Comment
 
-print('Finished Package Imports')
+print("Finished Package Imports!")
 
-parent_directory = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
+parent_directory = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
 sys.path.append(parent_directory)
 
 from project.utils.classes.tokenizer import Tokenizer
+
 
 if os.path.exists("wordvectors.kv"):
     word_vectors = KeyedVectors.load("wordvectors.kv")
 else:
     import gensim.downloader as api
+
     word_vectors = api.load("glove-twitter-200")
     word_vectors.save("wordvectors.kv")
 
-print('Loaded Word Vectors')
+print("Loaded Word Vectors!")
 
-def load_data(subreddit=None, utc_start=None, utc_end=None, rows=None):
-    
-    pickle_file_path_comment = os.path.join(parent_directory, 'project/data_collection/project_data/climateCommentsDf.pickle')
-    
-    with open(pickle_file_path_comment, 'rb') as file:
+
+def load_data(
+    filepath,
+    subreddit=None,
+    col_to_tokenize=None,
+):
+    pickle_file_path_comment = os.path.join(
+        parent_directory,
+        filepath,
+    )
+
+    with open(pickle_file_path_comment, "rb") as file:
         comments_df = pickle.load(file)
-    
+
     if subreddit:
-        comments_df = comments_df[comments_df['subreddit'] == subreddit]
-    
-    if utc_start and utc_end:
-        comments_df = comments_df[(comments_df['created_utc'] >= utc_start) & (comments_df['created_utc'] <= utc_end)]
-    
-    comments_df = comments_df[~comments_df['body'].isin(['[removed]', '[deleted]'])]
-    
-    if rows:
-        start_row, end_row = rows
-        comments_df = comments_df.iloc[start_row:end_row]
-    print(f'Filtered data for {subreddit}')
+        comments_df = comments_df[comments_df["subreddit"] == subreddit]
+
+    try:
+        token_key = f"tokenized_{col_to_tokenize.lower()}_words_norm"
+        comments_df = comments_df[
+            ~comments_df[token_key].isin(["[removed]", "[deleted]"])
+        ]
+    except:
+        print("Could not find removed or deleted entries")
+
+    if subreddit:
+        print(f"Filtered data for {subreddit}!")
+
+    else:
+        print("Filtered data!")
     return comments_df
 
-def tokenize_comments(df, subreddit):
-    pickle_path = f'comments_{subreddit}.pkl'
-    token_pickle_path = f'tokenized_comments_{subreddit}.pkl'
+
+def tokenize_comments(df, subreddit, col_to_tokenize):
+    pickle_path = f"comments_{subreddit}.pkl"
+    token_pickle_path = f"tokenized_comments_{subreddit}.pkl"
     df.to_pickle(pickle_path)
     tokenizer = Tokenizer(filepath=pickle_path, filename=token_pickle_path)
     tokenizer.df = df
-    tokenizer.process(cols_to_tokenize=[('body', 'tokenized_body')])
-    print('Tokenizer Complete')
+    tokenizer.process(
+        cols_to_tokenize=[
+            (col_to_tokenize, "tokenized_" + col_to_tokenize.lower())
+        ]
+    )
+    print("Tokenizer Complete!")
     return tokenizer.tokenized_df
 
-def compute_similarity(comment, foundation_words_vec):
+
+def compute_similarity(
+    comment, foundation_words_vec, similarity_threshold=0.25
+):
     similarities = []
     for word in comment:  # word in reddit comment
         try:
             word_vec = word_vectors[word]  # get the embedding
-            for foundation_word_vec in foundation_words_vec:  # loop through moral foundation words
-                sim = np.dot(word_vec, foundation_word_vec) / (np.linalg.norm(word_vec) * np.linalg.norm(foundation_word_vec))
-                similarities.append(sim)
-        except:
+            for (
+                foundation_word_vec
+            ) in foundation_words_vec:  # loop through moral foundation words
+                sim = np.dot(word_vec, foundation_word_vec) / (
+                    np.linalg.norm(word_vec)
+                    * np.linalg.norm(foundation_word_vec)
+                )
+                # Apply threshold
+                if sim >= similarity_threshold:
+                    similarities.append(sim)
+        except KeyError:  # If the word is not in the embedding vocabulary
             pass
+
     if similarities:
-        return np.mean(similarities) 
+        return np.mean(similarities)
     else:
         return 0
+
 
 def classify_sentence_with_profile(sentence, moral_foundations_dict):
     foundation_scores = {}
@@ -89,46 +120,106 @@ def classify_sentence_with_profile(sentence, moral_foundations_dict):
     return foundation_scores
 
 
-def main(subreddit=None, utc_start=None, utc_end=None, rows=None):
-    comments_df = load_data(subreddit=subreddit, utc_start=utc_start, utc_end=utc_end, rows=rows)
-    tokenized_comments = tokenize_comments(comments_df, subreddit)
-    
-    with open('expanded_moral_foundations_dictionary.json', 'r') as f:
+def main(
+    filepath,
+    subreddit=None,
+    tokenize=False,
+    col_to_tokenize=False,
+    type="Undefined",
+):
+    comments_df = load_data(
+        filepath=filepath,
+        subreddit=subreddit,
+    )
+
+    if tokenize is True:
+        tokenized_comments = tokenize_comments(
+            comments_df, subreddit, col_to_tokenize
+        )
+    else:
+        print("Columns Already Tokenized!")
+        tokenized_comments = comments_df
+
+    with open("expanded_moral_foundations_dictionary.json", "r") as f:
         word_to_moral_foundation_expanded = json.load(f)
-    
+
     # classify sentences
     classification_profiles = []
-    for comment in tokenized_comments['tokenized_body_words_norm']:
-        classification_profile = classify_sentence_with_profile(comment, word_to_moral_foundation_expanded)
+    token_key = f"tokenized_{col_to_tokenize.lower()}_words_norm"
+    for comment in tokenized_comments[token_key]:
+        classification_profile = classify_sentence_with_profile(
+            comment, word_to_moral_foundation_expanded
+        )
         classification_profiles.append(classification_profile)
-    print('Finished Classification')
+    print("Finished Classification!")
 
     # add classifications to original df
     df_classification_profiles = pd.DataFrame(classification_profiles)
-    result_df = pd.concat([tokenized_comments.reset_index(drop=True), df_classification_profiles.reset_index(drop=True)], axis=1)
-    
+    result_df = pd.concat(
+        [
+            tokenized_comments.reset_index(drop=True),
+            df_classification_profiles.reset_index(drop=True),
+        ],
+        axis=1,
+    )
+
     # write to pickle file
-    start_row, end_row = rows if rows else (None, None)
-    filename = f"result_{subreddit}_{utc_start}_{utc_end}_{start_row}_{end_row}.pkl"
+    filename = f"result_{subreddit}_{type}.pkl"
     result_df.to_pickle(filename)
-    print(f"Saved DataFrame to {filename}")
-    
+    print(f"Saved DataFrame to {filename}!")
+
     return result_df
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process subreddit comments.')
-    parser.add_argument('--subreddit', type=str, required=True, help='Subreddit name')
-    parser.add_argument('--utc_start', type=int, required=False, help='Start time in UTC')
-    parser.add_argument('--utc_end', type=int, required=False, help='End time in UTC')
-    parser.add_argument('--rows', type=str, required=False,help='Starting and ending row indices separated by comma')
+    parser = argparse.ArgumentParser(
+        description="Process comments or submissions."
+    )
+
+    parser.add_argument(
+        "--filepath",
+        type=str,
+        required=True,
+        help="Filepath to data",
+    )
+
+    parser.add_argument(
+        "--col_to_tokenize",
+        type=str,
+        help="Column to tokenize.",
+    )
+
+    parser.add_argument("--subreddit", type=str, help="Subreddit name")
+
+    parser.add_argument(
+        "--type", type=str, help="Token type (i.e. submission, comment)"
+    )
+
+    parser.add_argument(
+        "--tokenize",
+        type=str,
+        required=True,
+        help="True to tokenize data before classifying, false if data is already tokenized",
+    )
 
     args = parser.parse_args()
 
-    if args.rows:
-        rows = tuple(map(int, args.rows.split(',')))
+    # Handling optional string arguments that could be 'None'
+    args.subreddit = None if args.subreddit == "None" else args.subreddit
+    args.col_to_tokenize = (
+        None if args.col_to_tokenize == "None" else args.col_to_tokenize
+    )
+
+    if args.tokenize.lower() in ["true", "1", "t", "y", "yes"]:
+        tokenize = True
     else:
-        rows = None
+        tokenize = False
 
-    result_df = main(subreddit=args.subreddit, utc_start=args.utc_start, utc_end=args.utc_end, rows=rows)
+    result_df = main(
+        filepath=args.filepath,
+        col_to_tokenize=args.col_to_tokenize,
+        subreddit=args.subreddit,
+        tokenize=tokenize,
+        type=args.type,
+    )
     print(result_df.head())
-
---
+++ project/data_collection/moral_foundation_data.py
@@ -0,0 +1,92 @@
+import os
+
+import pandas as pd
+
+
+def load_comments():
+    parent_directory = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    directory = os.path.join(
+        parent_directory, "data_collection/project_data/results/comments/"
+    )
+    all_comments_tokenized_analyzed = directory_importer(directory)
+    all_comments_tokenized_analyzed = df_processor(
+        all_comments_tokenized_analyzed
+    )
+    return all_comments_tokenized_analyzed
+
+
+def load_submissions():
+    parent_directory = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    directory = os.path.join(
+        parent_directory, "data_collection/project_data/results/submissions/"
+    )
+    all_submissions_tokenized_analyzed = directory_importer(directory)
+    all_submissions_tokenized_analyzed = df_processor(
+        all_submissions_tokenized_analyzed
+    )
+    return all_submissions_tokenized_analyzed
+
+
+def directory_importer(directory):
+    final_df = pd.DataFrame()
+    for filename in os.listdir(directory):
+        if filename.endswith(".pkl"):
+            file_path = os.path.join(directory, filename)
+            df = pd.read_pickle(file_path)
+            final_df = pd.concat([final_df, df], ignore_index=True)
+    return final_df
+
+
+def df_processor(full_df):
+    full_df["Harm_Care_Agg"] = (full_df["HarmVice"] + full_df["HarmVirtue"]) / 2
+    full_df["Authority_Agg"] = (
+        full_df["AuthorityVice"] + full_df["HarmVirtue"]
+    ) / 2
+    full_df["Purity_Agg"] = (
+        full_df["PurityVice"] + full_df["PurityVirtue"]
+    ) / 2
+    full_df["Fairness_Agg"] = (
+        full_df["FairnessVice"] + full_df["FairnessVirtue"]
+    ) / 2
+    full_df["Ingroup_Agg"] = (
+        full_df["IngroupVice"] + full_df["IngroupVirtue"]
+    ) / 2
+    full_df["Dominant_Moral_Foundation"] = full_df[
+        [
+            "HarmVirtue",
+            "AuthorityVirtue",
+            "PurityVirtue",
+            "HarmVice",
+            "PurityVice",
+            "IngroupVice",
+            "FairnessVirtue",
+            "FairnessVice",
+            "IngroupVirtue",
+            "AuthorityVice",
+        ]
+    ].idxmax(axis=1)
+    full_df["Dominant_Moral_Foundation_Agg"] = full_df[
+        [
+            "Harm_Care_Agg",
+            "Authority_Agg",
+            "Purity_Agg",
+            "Fairness_Agg",
+            "Ingroup_Agg",
+        ]
+    ].idxmax(axis=1)
+    return full_df
+
+
+def main():
+    comments_df = load_comments()
+    submissions_df = load_submissions()
+
+    print("Comments DataFrame:")
+    print(comments_df.head())
+
+    print("\nSubmissions DataFrame:")
+    print(submissions_df.head())
+
+
+if __name__ == "__main__":
+    main()
